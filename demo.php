4293e795bff2de42dabd6c7894c1ec847dd56e5f
pramodchauhan1 <pramod@tingmail.in>
2023-02-08 19:52:42 +0530

    header created

---
+++ demo.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+    <?php include("includes/include_css.html") ?>
+</head>
+
+<body>
+    <?php include("includes/browser_upgrade.html") ?>
+    <?php include("includes/loader.html") ?>
+    <?php include("includes/header.html") ?>
+    <?php include("includes/sidebar.html") ?>
+
+    <main id="index_page" class="smooth_scroll_pages">
+        <!--  insert body content  -->
+
+        <div class="container">
+            <h2 class="text_xxxxl">Lorem, ipsum dolor</h2>
+            <h2 class="text_xxxl">Lorem, ipsum dolor</h2>
+            <h2 class="text_xxl">Lorem, ipsum dolor</h2>
+            <h2 class="text_xl">Lorem, ipsum dolor</h2>
+            <h2 class="text_lg">Lorem, ipsum dolor</h2>
+            <h2 class="text_md">Lorem, ipsum dolor</h2>
+            <p class="text_reg">Lorem, ipsum dolor</p>
+            <p class="text_sm">Lorem, ipsum dolor</p>
+            <p class="text_xs">Lorem, ipsum dolor</p>
+            <br>
+            <a class="btn_project_default" href="">Sign In </a>
+            <br>
+            <br>
+            <a class="btn_project_default black_btn" href="">Create Account</a>
+        </div>
+
+        <!--  end body content -->
+    </main>
+    <?php include("includes/include_js.html") ?>
+
+</body>
+
+</html>
